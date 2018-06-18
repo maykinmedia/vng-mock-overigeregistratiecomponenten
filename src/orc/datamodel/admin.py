ee4830fcae2ee9c8679aac2ad321f36e99b2b2fd
Sergei Maertens <sergei@maykinmedia.nl>
2018-06-18 12:24:51 +0200

    Setup domain data model

--- src/orc/datamodel/admin.py
+++ src/orc/datamodel/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from .models import DomeinData
+
+
+@admin.register(DomeinData)
+class DomeinDataAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
--- src/orc/datamodel/models.py
+++ src/orc/datamodel/models.py
@@ -1,1 +1,15 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
+
+
+class DomeinData(models.Model):
+    data = JSONField()
+
+    class Meta:
+        verbose_name = 'domeindata'
+        verbose_name_plural = 'domeindata'
+
+    def __str__(self) -> str:
+        if isinstance(self.data, dict):
+            return str(list(self.data.keys))[:50]
+        return str(self.data)[:50]
